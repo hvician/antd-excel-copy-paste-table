acec4987aaabf9e7514c3e9e03a68371b280506d
Henrich Vician <amun.ra.software@outlook.com>
2021-08-01 14:33:00 -0400

    table data utils

--- src/components/AntdExcelCopyPasteTable/utils.ts
+++ src/components/AntdExcelCopyPasteTable/utils.ts
@@ -1,7 +1,42 @@
 import { ClipboardEvent } from 'react'
+import { TableColumnType, TableProps } from 'antd'
+import { parse } from 'papaparse'
 
-export const pasteEvent = (pasteHandler: (values: any) => void, event: ClipboardEvent<any>): void => {
-  const value = event.clipboardData.getData('text/html')
+const parseConfig = {
+  header: true,
+  delimiter: '\t',
+}
 
-  pasteHandler(value)
+const makeColumns = (parsedData: unknown[]) => {
+  const keys = Object.keys(parsedData[0])
+
+  return keys
+    ? keys.map((key: string) => ({
+        title: key,
+        dataIndex: key,
+        key,
+      }))
+    : null
+}
+
+const makeTableData = (parsedData: unknown[]) =>
+  parsedData.map((item: Record<string, any>, index: number) => {
+    const keyedItem = item
+    keyedItem.key = index
+    return keyedItem
+  })
+
+export const pasteEvent = (
+  setColumns: (columnData: TableColumnType<unknown>[]) => void,
+  setTableData: (tableData: TableProps<unknown>[]) => void,
+  event: ClipboardEvent<any>
+): void => {
+  const value = event.clipboardData.getData('text/plain')
+  const parsedData = parse(value, parseConfig)
+
+  console.log(parsedData.data)
+  const cols = makeColumns(parsedData.data)
+  setColumns(cols)
+
+  setTableData(makeTableData(parsedData.data))
 }
